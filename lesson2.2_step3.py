431386fb234a5a214c8c37a1bb7c50032ea826c7
Svetalana Nasekina <life2109@yandex.ru>
2022-06-16 15:27:26 +0500

    16/06/22

--- lesson2.2_step3.py
+++ lesson2.2_step3.py
@@ -14,10 +14,10 @@ a_el = browser.find_element(By.XPATH, '/html/body/div/form/h2/span[2]')
 a = a_el.text
 b_el = browser.find_element(By.XPATH, '/html/body/div/form/h2/span[4]')
 b = b_el.text
-x2 = (int(a)+int(b))
+x2 = (int(a)+int(b))  #сложили два числа
 
 fg = browser.find_element(By.XPATH, '/html/body/div/form/div/select')
-fg.send_keys(x2)
+fg.send_keys(x2)     #выбрали сумму чисел в выпадашке
 
 browser.find_element(By.XPATH, '/html/body/div/form/button').click()
 
